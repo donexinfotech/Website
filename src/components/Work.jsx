907659bca6c313c12ef3c9af65d2d74220af1ed9
donexinfotech <donexinfotech@gmail.com>
2024-09-09 10:15:39 +0530

    Added new projects

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,6 +3,8 @@ import portfolio from './works/Screenshot 2024-08-04 120921.png'
 import report from './works/reportcard.png'
 import rbook from './works/rbook.png'
 import './work.css'
+import shyamsPortfolio from './works/portfolio.png'
+import a1 from "./works/a1.png";
 
 export default function Work() {
   return (
@@ -15,27 +17,35 @@ export default function Work() {
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
         </div>
         <div class="carousel-inner">
             <div class="carousel-item active">
-            <img src={portfolio} class="d-block caro-img mx-auto" alt="Work1"/>
+            <a href="https://ankit-mathapati.vercel.app"><img src={portfolio} class="d-block caro-img mx-auto" alt="Work1"/></a>
             <div class="carousel-caption d-none d-md-block">
                 <h5>Portfolio Website</h5>
                 <p>This was a portfolio website made for a client to showcase his skills and achievements</p>
             </div>
             </div>
             <div class="carousel-item">
-            <img src={report} class="d-block caro-img mx-auto" alt="..."/>
+            <a href="https://shyam-jet.vercel.app/#"><img src={shyamsPortfolio} class="d-block caro-img mx-auto" alt="..."/></a>
             <div class="carousel-caption d-none d-md-block caro-text">
-                <h5>Report Card Website</h5>
-                <p>This was a report card website made for one of our clients to list the results of all the students in the database</p>
+                <h5>Portfolio Website</h5>
+                <p>This was a portfolio website made for a client to showcase his skills and achievements</p>
             </div>
             </div>
             <div class="carousel-item">
-            <img src={rbook} class="d-block caro-img mx-auto" alt="..."/>
+            <a href="#"><img src={rbook} class="d-block caro-img mx-auto" alt="..."/></a>
             <div class="carousel-caption d-none d-md-block caro-text">
                 <h5>Recipe Book</h5>
                 <p>This is a Recipe book website made in order to showcase recipe's of verious foods in this website</p>
+            </div>
+            </div>
+            <div class="carousel-item">
+            <a href="https://a1-market.vercel.app/"><img src={a1} class="d-block caro-img mx-auto" alt="..."/></a>
+            <div class="carousel-caption d-none d-md-block caro-text">
+                <h5>A-1 Market</h5>
+                <p>An E-commerce store designed to buy items with functionality of cart user registration login etc...</p>
             </div>
             </div>
         </div>
